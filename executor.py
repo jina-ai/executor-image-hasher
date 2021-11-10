9e39267fc359a8f3b2a9b95d0f172fc63ccb10ed
makram93 <kalimakram@gmail.com>
2021-11-10 15:37:05 +0100

    fix: apply suggestions

--- executor.py
+++ executor.py
@@ -17,6 +17,7 @@ class ImageHasher(Executor):
     """
     An executer to encode the images using the `comparable` hashing techniques
     """
+
     def __init__(
         self,
         hash_type: str = 'perceptual',
@@ -30,11 +31,8 @@ class ImageHasher(Executor):
     ):
         """
         :param hash_type: the hashing technique used to encode the images. By default, set to `perceptual`.
-        Possible values are:
-        `perceptual` - http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html
-        `average` - https://web.archive.org/web/20171112054354/https://www.safaribooksonline.com/blog/2013/11/26/image-hashing-with-python/
-        `difference` - http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html
-        `wavelet` - https://www.kaggle.com/c/avito-duplicate-ads-detection/
+        Possible values are: `perceptual`, `average`, `difference`, and `wavelet`.
+        Implementation of the algorithm can be found here - https://github.com/JohannesBuchner/imagehash
         :param hash_size: the size of the encoded hash value. Should not be less than `2`
         :param average_hash_args: the arguments for the `average` hash encoding type. By default `avg_pixels`
         set to mean. The other possible value for `avg_pixels is `median`
@@ -51,7 +49,9 @@ class ImageHasher(Executor):
         """
         super().__init__(**kwargs)
         if not hash_type or hash_type not in HASH_TYPE:
-            raise ValueError('Please select one of the `hash_type`')
+            raise ValueError(
+                f'Please select one of the available `hash_type`: {HASH_TYPE}'
+            )
         self.hash_type = hash_type
         self.hash_size = hash_size
 
@@ -79,7 +79,7 @@ class ImageHasher(Executor):
     @requests
     def encode(self, docs: DocumentArray, parameters: Dict = {}, **kwargs):
         """
-        Read the numpy arrays from the Document.blob, and encode it into an embeddings using the
+        Read the numpy arrays from the Document.blob, and encode it into embeddings using the
         comparable hashing technique. Features in the image are used to generate a distinct
         (but not unique) fingerprint, and these fingerprints are comparable.
 
@@ -88,56 +88,65 @@ class ImageHasher(Executor):
             attribute should be the numpy array of the image, and should have dtype
             ``np.uint8``
         :param parameters: A dictionary that contains parameters to control encoding.
-            The accepted keys are: `perceptual_hash_args`, `average_hash_args`, `wavelet_hash_args`,
-            and `traversal_paths`.
-            For example: `parameters={'average_hash_args': {'avg_pixels': 'median'}}`
+            The accepted keys are: `hash_type`, `hash_size`, `perceptual_hash_args`,
+            `average_hash_args`, `wavelet_hash_args`, and `traversal_paths`.
+            For example: `parameters={'hash_type': 'average', 'average_hash_args': {'avg_pixels': 'median'}}`
         """
+        missing_blob = []
+        hash_type = parameters.get('hash_type', self.hash_type)
+        hash_size = parameters.get('hash_size', self.hash_size)
+
         for doc in docs.traverse_flat(
             parameters.get('traversal_paths', self.traversal_paths)
         ):
             if doc.blob is None:
-                self.logger.error(f'No blob passed for the Document with id: {doc.id}')
+                missing_blob.append(doc.id)
                 continue
 
             image = Image.fromarray(doc.blob)
             hash_hex = None
             try:
-                if self.hash_type == 'wavelet':
+                if hash_type == 'wavelet':
                     wavelet_hash_args = deepcopy(self._wavelet_hash_args)
                     wavelet_hash_args.update(parameters.get('wavelet_hash_args', {}))
                     hash_hex = imagehash.whash(
-                        image, hash_size=self.hash_size, **wavelet_hash_args
+                        image, hash_size=hash_size, **wavelet_hash_args
                     )
-                elif self.hash_type == 'average':
+                elif hash_type == 'average':
                     average_hash_args = deepcopy(self._average_hash_args)
                     average_hash_args.update(parameters.get('average_hash_args', {}))
                     hash_hex = imagehash.average_hash(
-                        image, hash_size=self.hash_size, **average_hash_args
+                        image, hash_size=hash_size, **average_hash_args
                     )
-                elif self.hash_type == 'perceptual':
+                elif hash_type == 'perceptual':
                     perceptual_hash_args = deepcopy(self._perceptual_hash_args)
                     perceptual_hash_args.update(
                         parameters.get('perceptual_hash_args', {})
                     )
                     hash_hex = imagehash.phash(
-                        image, hash_size=self.hash_size, **perceptual_hash_args
+                        image, hash_size=hash_size, **perceptual_hash_args
                     )
                 else:
-                    hash_hex = imagehash.phash(image, hash_size=self.hash_size)
+                    hash_hex = imagehash.phash(image, hash_size=hash_size)
             except AssertionError as e:
                 self.logger.error(f'Image hashing failed, {e}')
 
             if hash_hex is not None:
                 if self.is_embed_bool:
                     doc.embedding = np.squeeze(
-                        np.array(hash_hex.hash).reshape(1, self.hash_size ** 2)
+                        np.array(hash_hex.hash).reshape(1, hash_size ** 2)
                     )
                 else:
                     doc.embedding = np.squeeze(
                         np.packbits(
-                            np.array(hash_hex.hash).reshape(1, self.hash_size ** 2),
+                            np.array(hash_hex.hash).reshape(1, hash_size ** 2),
                             axis=1,
                         )
                     )
             else:
                 self.logger.warning(f'Could not set embeddings for {doc.id}')
+
+        if len(missing_blob) > 0:
+            self.logger.error(
+                f'No blob passed for the following Documents with ids: {missing_blob}'
+            )
